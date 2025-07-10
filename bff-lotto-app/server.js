5e17285efcab845bc2e0c3f8c844857876d6ab60
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-07-10 20:39:40 +0700

    Can set PORT from ENV name SERVER_PORT

--- bff-lotto-app/server.js
+++ bff-lotto-app/server.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const userRoutes = require('./src/api/routes/apiRouter');
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.SERVER_PORT || 3003;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/bff-lotto-app', userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`BFF running on http://localhost:${PORT}`);
+app.listen(SERVER_PORT, () => {
+  console.log(`BFF running on http://localhost:${SERVER_PORT}`);
 });
