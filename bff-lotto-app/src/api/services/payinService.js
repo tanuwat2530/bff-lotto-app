c9de6b63ebc751c32cb87dedfb9f023dff380f61
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-08-01 22:10:24 +0700

    deploy deposit

--- bff-lotto-app/src/api/services/payinService.js
+++ bff-lotto-app/src/api/services/payinService.js
@@ -1,9 +1,7 @@
 const axios = require('axios');
 
 exports.forwardToBackend = async (requestData) => {
-  console.log(requestData)
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  console.log(`${apiUrl}/gateway-api/pay-in`)
   const response = await axios.post(`${apiUrl}/gateway-api/pay-in`, requestData, {
     headers: { 'Content-Type': 'application/json' }
   });
