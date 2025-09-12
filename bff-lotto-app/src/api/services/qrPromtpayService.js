b4b5b5c8811ed173c70aca523cbbaa23bf278720
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-09-13 00:17:18 +0700

    Add qr promtpay deposit

--- bff-lotto-app/src/api/services/qrPromtpayService.js
+++ bff-lotto-app/src/api/services/qrPromtpayService.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 exports.forwardToBackend = async (requestData) => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  
   const response = await axios.post(`${apiUrl}/transfer-api/promtpay`, requestData, {
     headers: { 'Content-Type': 'application/json' }
   });
